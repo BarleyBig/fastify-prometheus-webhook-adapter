46284e3ed354ba3153b4f937781d7bee25ae8ebe
BarleyBig <baltoo@qq.com>
2022-01-12 15:19:45 +0800

    修改默认template的颜色展示

--- tmpls/default.js
+++ tmpls/default.js
@@ -2,7 +2,7 @@
 
 ${info.alerts.map(p => `--------------------------------  
 
-**告警级别:**  ${(info.status.toUpperCase() === 'RESOLVED' ? '💙' : { 'normal': '💛', 'yellow': '💜💜', 'red': '❤❤❤' })[p.labels.severity] ?? '💛'}  
+**告警级别:**  ${(info.status.toUpperCase() === 'RESOLVED' ? '💙' : { 'normal': '💛', 'yellow': '❤❤', 'red': '💜💜💜' })[p.labels.severity] ?? '💛'}  
 
 **触发时间:**   \`${dayjs(p.startsAt).format('YYYY-MM-DD HH:mm:ss')}\`  
 
